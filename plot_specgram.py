eff5396467e45a349272bddc18a3594016694814
Ankur <ankur.barsode@icts.res.in>
2024-02-19 10:44:21 +0530

    Changed title from GW jug to GW Chirp Bottle (Jalak Mehtas suggestion)

--- plot_specgram.py
+++ plot_specgram.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import read
 
-fname = 'GW_jug_4'
+fname = 'GW_jug_short'
 Fs, h = read('%s.wav' % fname)
 h = h[2*Fs:]
 
@@ -20,18 +20,21 @@ plt.clim([25,50])
 
 plt.plot(t, f, color='lawngreen', ls=':', lw=3, label=r'$m_1=m_2=%.2fM_\odot,\ t_c=%.1fs$' % (m1, tc))
 plt.text(1, 250, r'$m_1=m_2=%.2fM_\odot,\ t_c=%.1fs$' % (m1, tc), color='lawngreen', fontsize=15)
+
+'''
 plt.plot(t,f * 5.5, color='lawngreen', ls=':', lw=3, label='5.5$f_0$')
 plt.text(1, 850, '5.5$f_0$', color='lawngreen', fontsize=15)
 plt.plot(t,f * 9.5, color='lawngreen', ls=':', lw=3, label='9.5$f_0$')
 plt.text(1, 1400, '9.5$f_0$', color='lawngreen', fontsize=15)
 plt.plot(t,f * 13.5, color='lawngreen', ls=':', lw=3, label='13.5$f_0$')
 plt.text(1, 1800, '13.5$f_0$', color='lawngreen', fontsize=15)
+'''
 
 #plt.legend(loc='upper left', fontsize=13)
 #plt.yscale('log')
 #plt.colorbar()
-plt.xlabel(r'$t$ (s)')
-plt.ylabel(r'$f$ (Hz)')
+plt.xlabel(r'time (s)', fontsize=14)
+plt.ylabel(r'frequency (Hz)', fontsize=14)
 plt.tight_layout()
-plt.savefig('%s.png' % fname)
+plt.savefig('%s_new.png' % fname)
 plt.show()
